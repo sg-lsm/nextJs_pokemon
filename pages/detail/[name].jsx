1485ad66f22d162c2ebed122046109fcee59c41c
lsm <70528905+sg-lsm@users.noreply.github.com>
2023-11-08 16:38:44 +0900

    name, number, etcs

--- pages/detail/[name].jsx
+++ pages/detail/[name].jsx
@@ -1,6 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import css from 'styled-jsx/css';
 
 const pokeDetail = ({ pokeNum,name }) => {
 	/*
@@ -17,19 +18,60 @@ const pokeDetail = ({ pokeNum,name }) => {
 	let [_pokeInfo] = pokeInfo;
 	// let pokeImg_front = _pokeInfo.sprites.front_default;
 	let pokeImg_front = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name}.png`;
+	let pokeImg_back = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${name}.png`;
+	
 	// console.log(_pokeInfo);
 	// console.log(pokeInfo);
 
+	let types = [];
+	let type = [];
+	for (let i = 0; i < _pokeInfo.types.length; i++){
+		const path = _pokeInfo.types;
+		types.push({
+			slot : path[i].slot,
+			type : path[i].type
+		})
+	}
+	console.log(types[1].type);	
+	// types.type[1].name
+	// types.type[1].url
+
+	
+	
+	// console.log(type);
+
 	return (
 		<>
-			<Image src={pokeImg_front} width={240} height={240} />
+			<div className="poke_img_container">
+				<Image src={pokeImg_front} width={240} height={240} />
+				<Image src={pokeImg_back} width={240} height={240} />
+			</div>
+			<div className="poke_info_container">
+				<div className="info_name">name: {_pokeInfo.name}</div>
+				<div className="info_number">number: {_pokeInfo.order}</div>
+				<div className="info_height">height: {_pokeInfo.height}m</div>
+				<div className="info_weight">weight: {_pokeInfo.weight}kg</div>
+				<div className="info_base_exp">
+					base experience: {_pokeInfo.base_experience}
+				</div>
+			</div>
+
+			<style jsx>{style}</style>
 		</>
 	);
+
+	
 
 	//api => param => link /${name}
 	// name => to props => api get
 	// api => parsing to div/Link
 };
+
+const style = css`
+	.poke_img_container {
+		display: flex;
+	}
+`;
 
 export const getServerSideProps = async ({ query }) => {
 	// console.log(query);
